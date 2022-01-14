0fab874a14dcbf7e7c5c9d08dbbb89e185368b1d
YHx07 <dima-pavlov1999@yandex.ru>
2022-01-14 14:03:37 +0300

    1.2

--- sql/create_tables.sql
+++
@@ -1,8 +0,0 @@
--- Creation of product table
-CREATE TABLE IF NOT EXISTS booking (
-  id INT NOT NULL,
-  name varchar(250) NOT NULL,
-  dttm varchar(250) NOT NULL,
-  workplace varchar(250) NOT NULL,
-  PRIMARY KEY (id)
-);
